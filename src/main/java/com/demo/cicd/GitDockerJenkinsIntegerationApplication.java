19524c591e4582299792862c97bc5978d30ac46a
mohfaiza3 <mohammad.faizan@publicissapient.com>
2021-08-09 23:13:45 +0530

    first commit

---
+++ src/main/java/com/demo/cicd/GitDockerJenkinsIntegerationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDockerJenkinsIntegerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDockerJenkinsIntegerationApplication.class, args);
+	}
+
+}
